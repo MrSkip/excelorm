f1c1b739d53440837e3f5da35f3104e585704500
Dmytro Mula <dimon.mula@gmail.com>
2018-12-30 12:26:07 +0200

    updated java docs

--- src/main/java/com/sombrainc/excelorm/enumeration/CellStrategy.java
+++ src/main/java/com/sombrainc/excelorm/enumeration/CellStrategy.java
@@ -16,13 +16,13 @@ public enum CellStrategy {
 
     /**
      * Continue reading cells until first empty/null cell is found
-     * In this case iteration goes over rows (row by row)
+     * In this case iteration goes over rows (row by row if step is not defined)
      */
     COLUMN_UNTIL_NULL,
 
     /**
      * Continue reading cells until first empty/null cell is found
-     * In this case iteration goes over columns (column by column)
+     * In this case iteration goes over columns (column by column if step is not defined)
      */
     ROW_UNTIL_NULL
 
